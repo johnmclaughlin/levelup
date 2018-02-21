c1a39703c0758e8872d5960a1bf7a7329cec31aa
john mclaughlin <john.n.mclaughlin@gmail.com>
2018-02-21 16:13:08 -0500

    Sort JSON by Merchant Name

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,7 +6,13 @@ module.exports = {
     
         return axios.get(encodedURI)
             .then(function(response) {
-                return response.data;
+                // Alphabetically sort response by Merchant Name
+                const locations = response.data.sort(function(a, b){
+                    if(a.location.merchant_name.toLowerCase() < b.location.merchant_name.toLowerCase()) return -1
+                    if(a.location.merchant_name.toLowerCase() > b.location.merchant_name.toLowerCase()) return 1
+                    return 0
+                })
+                return locations;
             });
     }
 }
